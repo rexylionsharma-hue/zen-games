74919f614184700e52a697eae86608f330ea3654
rexylionsharma-hue <rexylionsharma@gmail.com>
2026-01-02 07:12:48 +0530

    Delete game.js

--- game.js
+++
@@ -1,9 +0,0 @@
-let score = 0;
-
-const scoreDiv = document.getElementById("score");
-const btn = document.getElementById("tapBtn");
-
-btn.addEventListener("click", () => {
-  score++;
-  scoreDiv.innerText = "Score: " + score;
-});
